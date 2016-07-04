466d7fcbc7bb89bcd32d508b6fa1d6141b35224d
Kim Hart <kimhart90@gmail.com>
2016-07-03 22:11:11 -0400

    file structure working

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var ejs = require('ejs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+
+var mongoose = require('mongoose');
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/myDb';
+mongoose.connect(mongoUrl);
+
+
+app.get('/', function(req, res){
+  res.send('hey there');
+});
+
+app.listen(process.env.PORT || 3000);
