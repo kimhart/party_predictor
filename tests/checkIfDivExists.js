3986b8f09a78608378819f4b4ded6956932780aa
Kim Hart <kimhart90@gmail.com>
2016-09-30 23:37:19 -0400

    gitignore + moved files

--- tests/checkIfDivExists.js
+++ tests/checkIfDivExists.js
@@ -5,19 +5,3 @@ describe('getDiv', function() {
         expect(d.nodeName).toBe('DIV');
     });
 });
-
-describe('getDivBg', function() {
-    var d = document.querySelector('.box');
-
-    it('Should be teal', function() {
-        expect(d.style.backgroundColor).toBe('teal');
-    });
-});
-
-describe('getDivAttribute', function() {
-    var d = document.querySelector('.box');
-
-    it('Should be bar', function() {
-        expect(d.getAttribute('foo')).toBe('bar');
-    });
-});
